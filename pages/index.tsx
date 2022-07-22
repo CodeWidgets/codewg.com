0f0d2ce460652d35892f506e2023fd868765c62f
Cooper Saye <coopersaye@gmail.com>
2022-07-22 16:19:44 -0700

    Put up coming soon message.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,17 @@ import styles from '../styles/pages/Index.module.scss';
 export default function Index() {
   return (
     <div className={styles.container}>
+      <div className={styles.center}>
+        <h1>CodeWidgets</h1>
+        <p>coming soon</p>
+        <a
+          href="https://twitter.com/intent/follow?screen_name=CodeWidgets"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Follow @CodeWidgets on Twitter for updates
+        </a>
+      </div>
     </div>
   );
 }
